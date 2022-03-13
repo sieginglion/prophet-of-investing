f5c9117832464070bec0f4dbe1044862db3eca97
sieginglion <sieginglion@gmail.com>
2022-03-13 21:57:37 +0800

    init

---
+++ shared.py
@@ -0,0 +1,60 @@
+import os
+import yaml
+import pickle
+import gspread
+import requests as r
+
+
+class DotDict(object):
+    def __init__(self, d):
+        for k, v in d.items():
+            setattr(self, k, DotDict(v) if isinstance(v, dict) else v)
+
+with open('config.yaml', 'r') as f:
+    config = DotDict(yaml.safe_load(f))
+
+
+def cached(func):
+    if os.path.isfile(func.__name__):
+        cache = pickle.load(open(func.__name__, 'rb'))
+    else:
+        cache = {}
+    def func_(*param):
+        if param not in cache:
+            cache[param] = func(*param)
+            pickle.dump(cache, open(func.__name__, 'wb'))
+        return cache[param]
+    return func_
+
+def Float(x):
+    try:
+        return float(x)
+    except:
+        return 0
+
+def get_invests_and_ratio(market):
+    first_step = gspread.service_account('service-account.json').open('First Step').get_worksheet(0)
+    invests = [row[0] for row in first_step.get({'Crypto': 'A11:A20', 'Stock': 'A35:A39'}[market])]
+    ratio = first_step.get('K6', value_render_option='UNFORMATTED_VALUE')[0][0]
+    return invests, ratio
+
+def filter_overvalued(market, symbols, ratio):
+    undervalued = []
+    for symbol in symbols:
+        resp = r.get(f'https://us-central1-generated-armor-274023.cloudfunctions.net/CalcTargetPrice?market={ market }&symbol={ symbol }&ratio={ ratio }')
+        if resp.status_code == 200 and resp.json()['undervalued']:
+            undervalued.append(symbol)
+    return undervalued
+
+def find_worst_and_better(market, symbol_to_score):
+    invests, ratio = get_invests_and_ratio(market)
+    ranking_list = [symbol for symbol, _ in sorted(symbol_to_score.items(), key=lambda x: x[1], reverse=True)]
+    worst_i = max(ranking_list.index(invest) for invest in invests)
+    better = [symbol for symbol in ranking_list[:worst_i + 1] if symbol not in invests]
+    return ranking_list[worst_i], filter_overvalued(market, better, ratio)
+
+def notify(text):
+    r.post(f'https://api.telegram.org/bot{ config.bot_token }/sendMessage', json={
+        'chat_id': 1075192674,
+        'text': text
+    }).json()
---
+++ suggest-cryptos.py
@@ -0,0 +1,41 @@
+import time
+import numpy as np
+import traceback
+from shared import *
+
+n_caps = config.crypto.box * 2
+
+def get_cryptos():
+    resp = r.get(f'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page={ config.crypto.top }')
+    return resp.json()
+
+def get_caps(crypto):
+    resp = r.get(f'https://api.coingecko.com/api/v3/coins/{ crypto["id"] }/market_chart?vs_currency=usd&days={ n_caps + 1 }&interval=daily')
+    chart = resp.json()
+    if abs(chart['prices'][-1][1] - 1) > 0.01:
+        return [cap[1] for cap in chart['market_caps'][-n_caps:]]
+    return []
+
+def calc_momentum(caps):
+    box = config.crypto.box
+    caps = np.convolve(np.array(caps), np.full(box, 1 / box), 'valid')
+    return caps[-1] - caps[0]
+
+def get_symbol_to_momentum():
+    symbol_to_momentum = {}
+    for crypto in get_cryptos():
+        caps = get_caps(crypto)
+        if len(caps) == n_caps and all(caps):
+            symbol = crypto['symbol'].upper()
+            symbol_to_momentum[symbol] = calc_momentum(caps)
+        time.sleep(1)
+    return symbol_to_momentum
+
+if __name__ == '__main__':
+    try:
+        symbol_to_momentum = get_symbol_to_momentum()
+        worst, better = find_worst_and_better('Crypto', symbol_to_momentum)
+        notify('\n'.join([worst] + better))
+    except:
+        err = traceback.format_exc()
+        print(err); notify(err)
