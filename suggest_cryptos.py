050f32ed7a1ce50b33b05fc9d4f90365294fff92
sieginglion <sieginglion@gmail.com>
2022-06-03 02:48:43 +0800

    price to gross ratio

--- suggest_cryptos.py
+++ suggest_cryptos.py
@@ -1,5 +1,6 @@
 import re
 import traceback
+from collections import defaultdict
 
 import arrow
 import requests as r
@@ -7,42 +8,56 @@ import requests as r
 from shared import *
 
 
-def get_symbols():
+def get_symbol_to_cap():
     resp = r.get(
         f'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page={ config.crypto.top }'
     )
-    return [crypto['symbol'].upper() for crypto in resp.json()]
+    return {crypto['symbol'].upper(): crypto['market_cap'] for crypto in resp.json()}
 
 
-def get_symbol_to_profit(symbols):
+def get_symbol_to_gross():
     resp = r.get('https://tokenterminal.com/terminal/metrics/protocol_revenue')
     ID = re.search('static/.{,30}/_buildManifest\.js', resp.text).group(0)[7:-18]
     resp = r.get(
+        f'https://tokenterminal.com/_next/data/{ ID }/terminal/metrics/revenue.json'
+    )
+    project_to_symbol = {
+        project['name']: project['symbol']
+        for project in resp.json()['pageProps']['projectsV2']
+    }
+    resp = r.get(
         f'https://tokenterminal.com/_next/data/{ ID }/terminal/metrics/protocol_revenue.json'
     )
-    data = resp.json()['pageProps']
-    project_to_symbol = {
-        project['name']: project['symbol'] for project in data['projectsV2']
-    }
     then = (
-        arrow.now('UTC')
+        arrow.utcnow()
         .shift(days=-(config.crypto.window + 1))
         .format('YYYY-MM-DDTHH:mm:ssZZ')
     )
-    symbol_to_profit = dict.fromkeys(symbols, 0)
-    for daily in data['protocolRevenueData']['daily']:
-        if (project := daily['project']) in project_to_symbol:
-            if (symbol := project_to_symbol[project]) in symbol_to_profit:
-                if daily['datetime'] < then and daily['revenue_protocol']:
-                    symbol_to_profit[symbol] += daily['revenue_protocol']
-    return symbol_to_profit
+    symbol_to_gross = defaultdict(float)
+    for daily in resp.json()['pageProps']['protocolRevenueData']['daily']:
+        if daily['datetime'] > then:
+            if (project := daily['project']) in project_to_symbol:
+                if gross := daily['revenue_protocol']:
+                    symbol_to_gross[project_to_symbol[project]] += gross
+    return symbol_to_gross
+
+
+def calc_symbol_to_pg(symbol_to_cap, symbol_to_gross):
+    symbol_to_pg = {}
+    for symbol, cap in symbol_to_cap.items():
+        if gross := symbol_to_gross.get(symbol):
+            symbol_to_pg[symbol] = cap / gross
+    return symbol_to_pg
 
 
 if __name__ == '__main__':
     try:
-        symbols = get_symbols()
-        symbol_to_profit = get_symbol_to_profit(symbols)
-        invests, betters = get_invests_and_betters('Crypto', symbol_to_profit)
+        symbol_to_cap = get_symbol_to_cap()
+        symbol_to_gross = get_symbol_to_gross()
+        symbol_to_pg = calc_symbol_to_pg(symbol_to_cap, symbol_to_gross)
+        invests, betters = get_invests_and_betters(
+            'Crypto', {symbol: -pg for symbol, pg in symbol_to_pg.items()}
+        )
         notify('\n'.join(invests + [''] + betters))
     except:
         notify(traceback.format_exc())
